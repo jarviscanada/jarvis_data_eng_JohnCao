543935312283524f9f1694e48d460994198b65fb
John Cao <johncy.cao@gmail.com>
2021-07-28 23:51:58 +0000

    add logger to jdbc

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,9 +1,16 @@
 package ca.jrvs.apps.jdbc;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class JDBCExecutor {
+  final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
 
   public static void main(String[] args) {
-    System.out.println("Hello Learning JDBC");
+    BasicConfigurator.configure();
+    JDBCExecutor jdbcExe = new JDBCExecutor();
+
+    jdbcExe.logger.info("Hello Learning JDBC");
   }
 
 }
